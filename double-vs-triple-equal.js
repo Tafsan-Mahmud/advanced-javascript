5e297895b3bdba3e0e541d550b0a0d4310b79ebf
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-02-03 02:16:13 +0600

    done

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,13 @@
+
+//** super intersting topic  */
+// modeul-->23-4 double equal (==) vs triple equal (===), implicit conversion
+
+const first =  22 ;
+const second = '22';
+if(first === second){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false') ;
+}
+
