978327603e2ac36a245bec725635601a7e8f2d5a
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-02-02 20:45:05 +0600

    done

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// falsy -->
+// 0
+// ""
+// false 
+// undefined
+// null
+// NaN
+//truthy -->
+// all srting is true
+// "0" , " " , [] ,{}
+
+
+let names = 12;
+
+if (names || names == 0){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
