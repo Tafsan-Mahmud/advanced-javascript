d156f501c5123f17c50888b1e3c7397973b64596
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-02-02 21:22:32 +0600

    done

---
+++ null-vs-undefind.js
@@ -0,0 +1,24 @@
+let pakhi ;
+console.log(pakhi);
+
+function add (num1 , num2){
+        var sum = (num1 * num2) ;
+        return ;
+}
+const result = add(5 ,5);
+console.log(result);
+
+
+function add (num1 , num2){
+   console.log(num1 , num2);
+}
+const result = add(10);
+console.log(result);
+
+const premik = {names:'sabbir' , age:17 , phone:'4245335'};
+console.log(premik.roll);
+const fun = undefined ;
+console.log(fun);
+
+const ages = [2 , 3 ,3 ,4];
+console.log(ages[11]);
